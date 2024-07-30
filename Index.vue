e2cf6dfc0c4849cd01ce708ccb5a393a5627b66d
Bpalaquin <Beldrian05@hotmail.com>
2024-07-30 13:14:32 -0500

    Index.vue

--- Index.vue
+++ Index.vue
@@ -1,57 +1,150 @@
 <template>
-  <div class="q-pa-md d-flex flex-column align-center justify-start">
-    <q-btn label="Crear Usuario" @click="openFormModal" color="primary" />
-    <List 
-      :usuarios="usuarios" 
-      :columns="columns" 
-      @edit-user="editUser" 
-      class="q-mt-md"
-    />
-    <FormModal 
-      v-model="isFormModalOpen" 
-      :selected-user="selectedUser"
-      @save-user="handleSaveUser" 
-      @close="closeFormModal"
-    />
-  </div>
+  <q-dialog v-model="isOpenLocal">
+    <q-card class="form-card">
+      <q-card-section>
+        <div class="text-h4 text-center q-mb-md">Formulario registro</div>
+        <q-form @submit.prevent="handleSubmit">
+          <q-input
+            v-model="formData.username"
+            label="Usuario de red"
+            outlined
+            rules="[val => !!val || 'Campo requerido']"
+            class="q-mb-md"
+          />
+          <q-input
+            v-model="formData.name"
+            label="Nombres"
+            outlined
+            rules="[val => !!val || 'Campo requerido']"
+            class="q-mb-md"
+          />
+          <q-input
+            v-model="formData.last_name"
+            label="Apellidos"
+            outlined
+            rules="[val => !!val || 'Campo requerido']"
+            class="q-mb-md"
+          />
+          <q-input
+            v-model="formData.email"
+            label="Correo Electrónico"
+            outlined
+            :rules="[val => !!val || 'Campo requerido', validateEmail]"
+            class="q-mb-md"
+          />
+          <q-select
+            filled
+            v-model="selectedClients"
+            :options="clients"
+            option-value="id"
+            option-label="name"
+            label="Selecciona un cliente"
+            class="q-mb-md"
+          />
+          <div class="row justify-end q-gutter-sm">
+            <q-btn
+              label="Guardar cambios"
+              type="submit"
+              color="primary"
+            />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-import List from './components/List.vue';
-import FormModal from './components/Form.vue';
+import { ref, onMounted } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+import { fetchClients , registerUserService } from './form/services';
+
+const props = defineProps({
+  isFormModalOpen: Boolean,
+  userData: Object,
+});
+
+const emit = defineEmits(['update:isFormModalOpen', 'save-user']);
+
+const isOpenLocal = ref(props.isFormModalOpen);
+onMounted(() => {
+  emit('update:isFormModalOpen', isOpenLocal.value);
+});
+
+const formData = ref({
+  username: '',
+  name: '',
+  last_name: '',
+  email: '',
+  password: '',
+  role_id: '',
+  is_da: true,
+  nit: '',
+  domain: '',
+
+});
+
+const selectedClients = ref(null);
+const clients = ref([]);
+
+//var clientes = await fetchClients();
 
 
-const usuarios = ref([]);
-const isFormModalOpen = ref(false);
-const selectedUser = ref(null);
+onMounted(async () => {
+  clients.value = await fetchClients();
+});
 
-const columns = computed(() => [
-  { label: 'ID', field: 'id', align: 'center' },
-  { label: 'Nombre', field: 'nombre', align: 'center' },
-  { label: 'Apellido', field: 'apellido', align: 'center' },
-  { label: 'Correo electrónico', field: 'correoElectronico', align: 'center' },
-  { label: 'Usuario de Red', field: 'usuarioRed', align: 'center' },
-  { label: 'Fecha creación', field: 'fechaCreacion', align: 'center' },
-]);
-
-function openFormModal() {
-  selectedUser.value = null;
-  isFormModalOpen.value = true;
-}
-
-function closeFormModal() {
-  isFormModalOpen.value = false;
-  selectedUser.value = null;
-}
-
-function handleSaveUser(user) {
-  const index = usuarios.value.findIndex(u => u.id === user.id);
-  if (index !== -1) {
-    usuarios.value[index] = user;
-  } else {
-    usuarios.value.push(user);
+onMounted(() => {
+  if (selectedClients.value !== null && clients.value.some(clients => clients === selectedClients.value)) {
+    formData.value.clients = selectedClients.value;
   }
-  closeFormModal();
+});
+
+async function handleSubmit() {
+  formData.value.clients = uuidv4();
+  formData.value.role_id = uuidv4();
+
+  const response = await registerUserService(formData.value);
+
+  if(response && response.success) {
+    closeModal();
+    console.log(response);
+    resetFormData();
+    emit('save-user', formData.value);
+  } else {
+    console.error('Error al registrar el usuario', response);
+  }
+}
+
+function resetFormData() {
+  Object.keys(formData.value).forEach(key => {
+    formData.value[key] = '';
+  });
+}
+
+
+function closeModal() {
+  isOpenLocal.value = false;
+}
+
+function validateEmail(value) {
+  const emailPattern = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  return emailPattern.test(value) || 'Por favor ingrese un correo electrónico válido';
 }
 </script>
+
+<style scoped>
+.form-card {
+  width: 80%;
+  max-width: 500px;
+  margin: auto;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.text-h4 {
+  color: #333;
+}
+
+</style>
